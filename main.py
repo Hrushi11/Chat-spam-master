f5c44cec37c4cb9bba250900452fed03fc50664f
Hrushikesh <hrushiskachgunde@gmail.com>
2021-05-18 14:51:58 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import time
 
 Keyboard = Controller()
 time.sleep(5)
-while True:
+a = <"Number_of_times_you_want_to_spam"">
+while a>0:
     for letter in "<message_you_want_to_spam>":
         Keyboard.press(letter)
         Keyboard.release(letter)
     Keyboard.press(Key.enter)
     Keyboard.release(Key.enter)
+    a -= 1
 

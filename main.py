0ee8029cd2688546deba6d9a82d1f5271b936ae6
Hrushikesh <hrushiskachgunde@gmail.com>
2021-05-18 14:55:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import time
 
 Keyboard = Controller()
 time.sleep(5)
-a = <"Number_of_times_you_want_to_spam"">
+a = <"Number_of_times_you_want_to_spam"> #Change this as per your choice
 while a>0:
-    for letter in "<message_you_want_to_spam>":
+    for letter in "<message_you_want_to_spam>": #Change this as per your choice
         Keyboard.press(letter)
         Keyboard.release(letter)
     Keyboard.press(Key.enter)
